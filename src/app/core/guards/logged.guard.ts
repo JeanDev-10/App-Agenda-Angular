bdc4594a2ce199703ac00426b1b4d5d4f1d11d60
jeanDev-10 <jeanpierrerodriguezzambrano@gmail.com>
2024-07-23 21:38:26 -0500

    fix: :fire: importe logged guard

--- src/app/core/guards/logged.guard.ts
+++ src/app/core/guards/logged.guard.ts
@@ -1,6 +1,6 @@
-// import { CanActivateFn } from '@angular/router';
+ import { ActivatedRouteSnapshot, CanActivateFn } from '@angular/router';
 
-/*export const loggedGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state) => {
+export const loggedGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state) => {
   //logica de guardia para saber si está logeado o no
   return true;
-}; */
+};
