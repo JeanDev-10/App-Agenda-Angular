d10a969ec9dc2ff15014e9bd7d56223afc96ce17
Jahir Celorio <jahir.celorio200@gmail.com>
2024-07-27 19:37:55 -0500

    style: :sparkles: styles add in register

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { LottieComponent, AnimationOptions } from 'ngx-lottie';
 
 @Component({
@@ -9,6 +10,10 @@ import { LottieComponent, AnimationOptions } from 'ngx-lottie';
   styleUrl: './login.component.scss'
 })
 export default class LoginComponent {
+  constructor(private router:Router) {
+
+   }
+
   options: AnimationOptions = {
     path: 'anim/login_animation.json',
   };
@@ -17,4 +22,8 @@ export default class LoginComponent {
     maxWidth: '700px',
     margin: '0 auto',
   };
+
+  GotoRegister(){
+    this.router.navigate(['auth/register'])
+  }
 }
--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { LottieComponent, AnimationOptions } from 'ngx-lottie';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [LottieComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export default class RegisterComponent {
 
+  constructor(private router:Router) { 
+
+  }
+
+  options: AnimationOptions = {
+    path: 'anim/register_animation.json',
+  };
+
+  styles: Partial<CSSStyleDeclaration> = {
+    maxWidth: '700px',
+    margin: '0 auto',
+  };
+
+  // Ir a la pagina de login
+
+  Gotologin(){
+    this.router.navigate(['auth/login']);
+  }
 }
