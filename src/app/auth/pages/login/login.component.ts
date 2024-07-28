60eee927c5b7816ac67672f1d5c00a103e710032
Jahir Celorio <jahir.celorio200@gmail.com>
2024-07-27 19:22:52 -0500

    feat: :sparkles: add animated in login and design

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { LottieComponent, AnimationOptions } from 'ngx-lottie';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [LottieComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export default class LoginComponent {
+  options: AnimationOptions = {
+    path: 'anim/login_animation.json',
+  };
 
+  styles: Partial<CSSStyleDeclaration> = {
+    maxWidth: '700px',
+    margin: '0 auto',
+  };
 }
