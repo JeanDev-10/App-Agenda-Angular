691535ef53ee711f92a442c9ed742aae8381cbcf
Jahir Celorio <jahir.celorio200@gmail.com>
2024-08-08 12:26:29 -0500

    feat: :sparkles: Logic added in interceptor for token

--- src/app/core/interceptor/token.interceptor.ts
+++ src/app/core/interceptor/token.interceptor.ts
@@ -1,5 +1,10 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpHeaders } from '@angular/common/http';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  // Creamos una constante para setear los header y poder enviar el token de tipo bearer
+  const headers = new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('token')}`);
+
+  const reqClone = req.clone({headers});
+
+  return next(reqClone);
 };
